3156b898e892570b9588473eca41c05dd6de23bd
Irham Baehaqi <irhambaebaehaqi@gmail.com>
2024-03-30 05:36:04 +0700

    first commit

---
+++ JavaApplication12/src/javaapplication12/Main.java
@@ -0,0 +1,27 @@
+package javaapplication12;
+
+public class Main {
+    public static void main(String[] args) {
+        String kalimat = "Kamu siapa?";
+        int angka = 1245;
+        float decimal = 3.4f;
+        char kar = 'A';
+        int x = 1;
+        int y = 1;
+        int z = x + y;
+        System.out.println(z);
+        System.out.println("Hello Word");
+        System.out.println("Hello Word");
+        System.out.println("Nama Saya Irham Baehaqi");
+        String first = "Irham ";
+        String last = "Baehaqi ";
+        String fullname = first + last;
+        System.out.println(fullname);
+        System.out.println(3 + 3);
+        System.out.println(8 * 9 + 34);
+        System.out.println(kalimat);
+        System.out.println(angka);
+        System.out.println(decimal);
+        System.out.println(kar);
+    }
+}
